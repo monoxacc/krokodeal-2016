e6c5a6ae99af0ca89ec0a4525d216e7799c2db09
monoxacc <monoxacc@gmail.com>
2016-12-12 21:43:20 +0100

    downlaodURL

--- Krokodeal.user.js
+++ Krokodeal.user.js
@@ -2,7 +2,7 @@
 // @name        Krokodeal-Jäger (by MoNoX)
 // @namespace   Krokodeal2016
 // @description Absahnen!
-// @updateURL   
+// @downloadURL https://github.com/monoxacc/krokodeal-2016/raw/master/Krokodeal.user.js
 // @include     https://www.mydealz.de/*
 // @exclude     https://www.mydealz.de/xmas-game*
 // @require     https://gist.githubusercontent.com/arantius/3123124/raw/grant-none-shim.js
