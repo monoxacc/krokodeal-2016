13f91036fda7753af0c8e122c46e16b9714eb1b0
monoxacc <monoxacc@gmail.com>
2016-12-12 22:52:16 +0100

    Deleted Meta Reference

--- Krokodeal.user.js
+++ Krokodeal.user.js
@@ -3,12 +3,11 @@
 // @namespace   Krokodeal2016
 // @description Absahnen!
 // @downloadURL https://github.com/monoxacc/krokodeal-2016/raw/master/Krokodeal.user.js
-// @updateURL   https://github.com/monoxacc/krokodeal-2016/raw/master/Krokodeal.meta.js
 // @include     https://www.mydealz.de/*
 // @exclude     https://www.mydealz.de/xmas-game*
 // @require     https://gist.githubusercontent.com/arantius/3123124/raw/grant-none-shim.js
 // @require     https://raw.githubusercontent.com/eligrey/FileSaver.js/master/FileSaver.js
-// @version     2016.11
+// @version     2016.10
 // @grant       none
 // ==/UserScript==
 //   /==========\
