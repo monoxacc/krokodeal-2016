701f0e33f701dbea889b528a428592354a6e3112
monoxacc <monoxacc@gmail.com>
2018-08-08 19:27:20 +0200

    FileSaver fix

--- Krokodeal.user.js
+++ Krokodeal.user.js
@@ -8,8 +8,8 @@
 // @exclude     https://www.mydealz.de/xmas-game*
 // @exclude     https://www.mydealz.de/pepper-festival*
 // @require     https://gist.githubusercontent.com/arantius/3123124/raw/grant-none-shim.js
-// @require     https://raw.githubusercontent.com/eligrey/FileSaver.js/master/FileSaver.js
-// @version     2018.001
+// @require     https://raw.githubusercontent.com/eligrey/FileSaver.js/master/src/FileSaver.js
+// @version     2018.002
 // @grant       none
 // ==/UserScript==
 //   /==========\
